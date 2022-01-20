fa59a63c461c09421546956035e2adae99c98871
AishwaryaKhonde <89783234+AishwaryaKhonde@users.noreply.github.com>
2022-01-20 12:01:48 +0530

    Add files via upload

---
+++ Demo_SpringBoot_Testing/src/main/java/com/th/exception/ThbsBankException.java
@@ -0,0 +1,13 @@
+package com.th.exception;
+/**
+ * 
+ * @author aishwarya_khonde
+ *
+ */
+public class ThbsBankException extends Exception {
+	private static final long serialVersionUID = 1L;
+
+	public ThbsBankException(String message) {
+		super(message);
+	}
+}
